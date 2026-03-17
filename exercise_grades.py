7db97dd3577c9f6ec33ec1c4a26de95aab2c6f0a
ellatell <etell@mail.austral.edu.ar>
2026-03-17 12:17:43 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -11,7 +11,7 @@ def grades():
     nota1 = 8
     nota2 = 7
     nota3 = 9 
-  promedio = (nota1 + nota2 + nota3) / 3
+    promedio = (nota1 + nota2 + nota3) / 3
     nota_max = max(nota1, nota2, nota3)
     nota_min = min(nota1, nota2, nota3)
     puntos_faltan = 10 - promedio
