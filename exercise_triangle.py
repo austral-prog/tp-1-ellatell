fc7ab12a71c3ba0d3896aea3b85a4496b6ef5f24
ellatell <etell@mail.austral.edu.ar>
2026-03-17 11:44:02 -0300

    Update exercise_triangle.py

--- exercise_triangle.py
+++ exercise_triangle.py
@@ -7,5 +7,5 @@ def triangle():
     """
     base = 10
     altura = 6
- area_triangulo = ((base * altura) /2) 
-    print (area_triangulo)
+  area_triangulo = (base * altura) / 2
+    print(area_triangulo)
