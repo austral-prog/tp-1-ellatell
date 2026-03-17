0ec10b88687080aae4c43a1970e7f40595d54348
ellatell <etell@mail.austral.edu.ar>
2026-03-17 10:55:03 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -15,10 +15,17 @@ def math():
     b = 7 
 
 suma = a + b 
+print(suma)
     resta = a - b 
+print(resta) 
     producto= a * b 
+print(producto) 
     promedio = (a+b)/2 
+print(promedio) 
     cociente = a / b 
+print(cociente) 
     resto = a % b 
+print(resto) 
     valor = a//b 
+print(valor) 
 
