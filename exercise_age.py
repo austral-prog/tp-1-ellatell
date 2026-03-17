0914bd50615cab0b084f3cfae2ecbcb3f2eeabde
ellatell <etell@mail.austral.edu.ar>
2026-03-17 11:47:04 -0300

    Update exercise_age.py

--- exercise_age.py
+++ exercise_age.py
@@ -8,7 +8,7 @@ def age():
     3. La edad en horas (1 día = 24 horas)
     4. La edad en minutos (1 hora = 60 minutos)
     """
-   edad_anos = 25
+    edad_anos = 25
     edad_meses = 12 * edad_anos
     print (edad_meses)
     edad_dias = 365 * edad_anos
